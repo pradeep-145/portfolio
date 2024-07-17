d8658bd1b7121cd9882c507fc5c9a653f5ab8eae
pradeep sakthi 👋 <117750940+pradeep-145@users.noreply.github.com>
2024-07-17 17:51:40 +0530

    1

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,7 +5,6 @@ import './App.css'
 import Home from './Home'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
