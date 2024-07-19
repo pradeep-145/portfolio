2bfdda4d59f00edf4e6409f15decc59dc0898490
pradeep sakthi 👋 <117750940+pradeep-145@users.noreply.github.com>
2024-07-19 15:49:08 +0530

    name

--- portfolio/src/Home.jsx
+++ portfolio/src/Home.jsx
@@ -3,23 +3,29 @@ import './home.css'
 const Home = () => {
   return (
     <>
-      <nav>
+      <nav >
         <ul>
 
         <li>
 
-        <a><img src="./profile.png" alt="logo" /></a>
+        <a className='container'>
+          <img src="./profile.png" alt="logo"className='logo-container' />
+        <span className='name'> Pradeep Sakthi</span> 
+        </a>
         </li>
-        <li> <a href="">home</a></li>
+        <li> <a href="">Projects</a></li>
         </ul>
       </nav>
       <div className="aboutme mt-5 row">
         <div className="text col-md-7">
           <p></p>
         </div>
-      <div className="container col-md-5">
-  <img src="./image.png" className="img-fluid" style={{"border-radius":"10px"}} alt="Responsive image" />
-</div>
+        <div className="image col-md-5">
+            <img src="./image.png" className=" img-fluid" style={{"border-radius":"10px"}} alt="Responsive image" />
+        </div>
+
+      </div>
+      <div className="skills mt-5-row">
 
       </div>
     </>
