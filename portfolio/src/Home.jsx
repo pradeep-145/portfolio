8b8b3cb1e8b3b08b6cc7c6fef01833f1fb0f7556
pradeep sakthi 👋 <117750940+pradeep-145@users.noreply.github.com>
2024-07-19 12:33:09 +0530

    image

--- portfolio/src/Home.jsx
+++ portfolio/src/Home.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 import './home.css'
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <nav>
+        <ul>
+
+        <li>
+
+        <a><img src="./profile.png" alt="logo" /></a>
+        </li>
+        <li> <a href="">home</a></li>
+        </ul>
+      </nav>
+      <div className="aboutme mt-5 row">
+        <div className="text col-md-7">
+          <p></p>
+        </div>
+      <div className="container col-md-5">
+  <img src="./image.png" className="img-fluid" style={{"border-radius":"10px"}} alt="Responsive image" />
+</div>
+
+      </div>
+    </>
   )
 }
 
